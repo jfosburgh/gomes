e330543af4d330a4aa6850a4eeca829bf173177d
James Fosburgh <jwfosburgh@gmail.com>
2024-07-10 20:22:58 -0400

    pawn move & attack generation

--- pkg/chess/bitboard.go
+++ pkg/chess/bitboard.go
@@ -41,10 +41,62 @@ func (b BitBoard) AllPieces() uint64 {
 	return result
 }
 
+func (b BitBoard) SidePieces(side int) uint64 {
+	result := uint64(0)
+	for piece, pieceBoard := range b {
+		if (piece >> 3) == (side >> 3) {
+			result = result | pieceBoard
+		}
+	}
+
+	return result
+}
+
+func (b BitBoard) PawnMoves(side int) (uint64, uint64) {
+	enemyBitboard := b.SidePieces(((^side >> 3) & 0b1) << 3)
+	selfBitboard := b.SidePieces(side)
+	validTargets := enemyBitboard & (^selfBitboard)
+
+	potentialAttacks := uint64(0)
+	if side == WHITE {
+		potentialAttacks = (^fileMask(1) & b[side|PAWN]) << NORTHWEST
+		potentialAttacks = potentialAttacks | ((^fileMask(8) & b[side|PAWN]) << NORTHEAST)
+	} else {
+		potentialAttacks = (^fileMask(1) & b[side|PAWN]) >> NORTHWEST
+		potentialAttacks = potentialAttacks | ((^fileMask(8) & b[side|PAWN]) >> NORTHEAST)
+	}
+
+	var singleAdvance, doubleAdvance uint64
+	pawns := b[side|PAWN]
+	doubleAdvanceable := pawns & (rankMask(2) | rankMask(7))
+	if side == WHITE {
+		singleAdvance = pawns << 8
+		doubleAdvance = doubleAdvanceable << 16
+	} else {
+		singleAdvance = pawns >> 8
+		doubleAdvance = doubleAdvanceable >> 16
+	}
+
+	return potentialAttacks & validTargets, (singleAdvance | doubleAdvance) & (^b.AllPieces())
+}
+
 func (b BitBoard) Remove(piece, position int) {
 	b[piece] = b[piece] & (^(0b1 << position))
 }
 
 func (b BitBoard) Add(piece, position int) {
 	b[piece] = b[piece] | (0b1 << position)
+}
+
+func fileMask(file int) uint64 {
+	res := uint64(0)
+	for i := range 8 {
+		res = res | 1<<(i*8+file-1)
+	}
+
+	return res
+}
+
+func rankMask(rank int) uint64 {
+	return 0b11111111 << ((rank - 1) * 8)
 }
